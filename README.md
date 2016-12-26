b128c7e45ce5db8d22112cac9ca517859c4c6c3a
Giang <giang2b@gmail.com>
2016-12-26 12:37:23 +0700

    Signed-off-by: Giang <giang2b@gmail.com>

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ btcclub
 
 netstat -tulapn|grep ip:3000
 ===
+yum install gcc-c++ make
+sudo yum install openssl-devel
+curl --silent --location https://rpm.nodesource.com/setup_7.x | sudo -E bash -
+yum -y install nodejs
+git clone https://github.com/isaacs/npm.git
+cd npm
+sudo make install
--- public/const.php
+++ public/const.php
@@ -5,7 +5,7 @@ class BLC {
 	const WALLET_ID = 'ZjI3OGY2YmQtYzAwZC00NWZhLTg3OWUtNjk2ZWM4ZWYwOGRi';
 	const WALLET_PWD = 'Z2lhbmdudDc3MDA3Nw==';// Z2lhbmdudDc3MDA3Nw== //Z1A5OXpYWFM3NzAwNzc=
 	const WALLET_PWD2 = '123456';
-	const SERVICE_URL = 'http://45.32.255.50:3000/';// http://45.32.255.50:3000
+	const SERVICE_URL = 'http://127.0.0.1:3000/';// http://45.32.255.50:3000
 	const WALLET_PIN_ADDRESS = '1P31kHNco4F7EvvXzALEKaK9N3P7iofsVi';
 	const FEE = '0.00001'; // 0.00001 // 0.00019361
 	const STOCK_ID = '1CgLUw9fEStxLnJdUGwa2FSBNexfiYvV1r';
